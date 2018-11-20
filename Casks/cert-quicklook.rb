2209f11ddbfbba6eb74614dde8ba5a5ac1f4ed7c
Tim Hordern <tim.hordern@gmail.com>
2018-11-20 15:39:52 +1100

    Update Cert-QuickLook link as Google has archived the Google Code artifacts

--- Casks/cert-quicklook.rb
+++ Casks/cert-quicklook.rb
@@ -2,7 +2,8 @@ cask 'cert-quicklook' do
   version '3'
   sha256 '9fd0508ffb1731b52e6a0b5a9a168ede62b1ad17b591bc42c66efa628877e540'
 
-  url "https://cert-quicklook.googlecode.com/files/CertQuickLook-v#{version}.qlgenerator.zip"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/cert-quicklook/CertQuickLook-v#{version}.qlgenerator.zip"
+
   name 'cert-quicklook'
   homepage 'https://code.google.com/p/cert-quicklook/'
 
