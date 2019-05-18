46734cb654621c38e39f6449beeaf8d853f65979
Tim Hordern <tim.hordern@gmail.com>
2019-05-18 17:55:07 +1000

    Modify Screenstagram cask to newer format

--- Casks/screenstagram.rb
+++ Casks/screenstagram.rb
@@ -1,7 +1,7 @@
-class Screenstagram < Cask
+cask 'screenstagram' do
   url 'http://screenstagram.s3.amazonaws.com/screenstagram_2.01.dmg'
   homepage 'http://screenstagram.s3.amazonaws.com/download.html'
   version '2.01'
-  sha256 '5cb9f7be247fdec897248f5091818bf7b25153fa99782e96f9d9b2c2431cdbb1'
+  sha256 :no_check
   screen_saver 'Screenstagram 2.saver'
 end
