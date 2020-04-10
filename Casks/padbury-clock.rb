dab4e88e27283302e61f2ae8da82336c4d6d159c
Tim Hordern <tim.hordern@gmail.com>
2020-04-10 20:45:00 +1000

    Update cask format for Padbury Clock screensaver

--- Casks/padbury-clock.rb
+++ Casks/padbury-clock.rb
@@ -1,7 +1,7 @@
 # https://github.com/Homebrew/homebrew-cask/blob/5cf7b9f23d7fb7e6f2bab22d32f52f9a9d57a2de/Casks/padbury-clock.rb
 # This cask is modified from the original blob to deal with fork created by Chris Short to resurrect this screensaver.
 
-cask :v1 => 'padbury-clock' do
+cask 'padbury-clock' do
   version :latest
   sha256 :no_check
 
