8d0efc7fe76db2db8d7ba57b7b518b6f668c5791
Tim Hordern <tim.hordern@gmail.com>
2020-04-10 20:41:32 +1000

    Fix whitespace

--- Casks/font-league-script.rb
+++ Casks/font-league-script.rb
@@ -1,11 +1,11 @@
-cask 'font-league-script' do	
-  version :latest	
-  sha256 :no_check	
+cask 'font-league-script' do
+  version :latest
+  sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask	
-  url 'https://github.com/google/fonts/raw/master/ofl/leaguescript/LeagueScript.ttf'	
-  name 'League Script'	
-  homepage 'https://www.google.com/fonts/specimen/League+Script'	
+  # github.com/google/fonts was verified as official when first introduced to the cask
+  url 'https://github.com/google/fonts/raw/master/ofl/leaguescript/LeagueScript.ttf'
+  name 'League Script'
+  homepage 'https://www.google.com/fonts/specimen/League+Script'
 
-  font 'LeagueScript.ttf'	
+  font 'LeagueScript.ttf'
 end
