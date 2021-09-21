b9c1bc14a82c334dfba688937090faabf07855fc
Tim Hordern <tim.hordern@gmail.com>
2021-09-21 10:51:47 +1000

    Convert apostrophes to quotes to allow for future inline regex

--- Casks/soulver2.rb
+++ Casks/soulver2.rb
@@ -1,20 +1,21 @@
-cask 'soulver2' do
-  version '2.9.0-6104'
-  sha256 '2aaa67f6c08e1aa540d9314ef2abb72195b92b618a81366ff57386dc443b77f3'
+cask "soulver2" do
+  version "2.9.0-6104"
+  sha256 "2aaa67f6c08e1aa540d9314ef2abb72195b92b618a81366ff57386dc443b77f3"
 
-  url 'https://soulver.app/download-soulver-2', referer: 'https://documentation.soulver.app/faq'
-  name 'Soulver'
-  desc 'Notepad with a built-in calculator'
-  homepage 'https://soulver.app/'
+  url "https://soulver.app/download-soulver-2", referer: "https://documentation.soulver.app/faq"
+  name "Soulver 2"
+  desc "Notepad with a built-in calculator"
+  homepage "https://soulver.app/"
 
   auto_updates false
-  depends_on macos: '>= :yosemite'
+  depends_on macos: ">= :yosemite"
 
-  app 'Soulver 2.app'
+  app "Soulver 2.app"
 
   zap trash: [
-    '~/Library/Application Support/Soulver',
-    '~/Library/Preferences/com.acqualia.soulver.plist',
-    '~/Library/Autosave Information/Unsaved Soulver Document*',
+    "~/Library/Application Support/Soulver",
+    "~/Library/Preferences/com.acqualia.soulver.plist",
+    "~/Library/Autosave Information/Unsaved Soulver Document*",
   ]
 end
+
