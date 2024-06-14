eca4e55fd0b1174ad06d27ddfb85a884b9993a75
Tim Hordern <timhordern@Tims-MBP.localdomain>
2024-06-14 14:36:59 +1000

    Add Alfred 2.9 as per Alfred 2 changelog https://www.alfredapp.com/changelog/v2/
    Note that upstream homebrew-cask did not adopt the intermediary versions of 2.8.5, 2.8.6, 2.8.7, 2.8.8, 2.8.9, or 2.9 and instead jumped to 3.0 as per https://github.com/Homebrew/homebrew-cask/blob/2cea18335a8acf90594420f1c34d24087875e9fd/Casks/alfred.rb

---
+++ Casks/alfred29.rb
@@ -0,0 +1,30 @@
+cask 'alfred29' do
+  version '2.9_446'
+  sha256 'c22e74e0cf2ea82d35cb4c81b5408dd29f573324cb2aa924a4bc3158af8d033a'
+
+  url "https://cachefly.alfredapp.com/Alfred_#{version}.zip"
+  name 'Alfred'
+  homepage 'https://www.alfredapp.com/'
+  license :freemium
+
+  auto_updates true
+  accessibility_access true
+
+  app 'Alfred 2.app'
+
+  postflight do
+    suppress_move_to_applications key: 'suppressMoveToApplications'
+  end
+
+  uninstall quit:       'com.runningwithcrayons.Alfred-2',
+            login_item: 'Alfred 2'
+
+  zap delete: [
+                '~/Library/Application Support/Alfred 2',
+                '~/Library/Caches/com.runningwithcrayons.Alfred-2',
+                '~/Library/Caches/com.runningwithcrayons.Alfred-Preferences',
+                '~/Library/Preferences/com.runningwithcrayons.Alfred-2.plist',
+                '~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist',
+                '~/Library/Saved Application State/com.runningwithcrayons.Alfred-Preferences.savedState',
+              ]
+end
