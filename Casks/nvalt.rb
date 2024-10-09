64199e72208b7e044045be35cb85d2ec8af21a1e
Tim Hordern <tim@timhordern.com>
2024-10-09 13:57:07 +1100

    Fix Github download URL for nvALT 2 due to inconsistencies in naming of release on Github

--- Casks/nvalt.rb
+++ Casks/nvalt.rb
@@ -2,7 +2,8 @@ cask "nvalt" do
     version '2.2.8-128'
     sha256 "85420c2a8d505a580b4aa4f0ef4662f08aa4af6139fb4ed448752b6b6e8fd671"
 
-    url "https://github.com/ttscoff/nv/releases/download/v#{version}/nvALT.#{version.no_hyphens}.dmg"
+    # https://github.com/ttscoff/nv/releases/download/2.2.8/nvALT2.2.8128.dmg
+    url "https://github.com/ttscoff/nv/releases/download/#{version.sub(/\-\d+/, "")}/nvALT.#{version.no_hyphens}.dmg"
     name "nvALT"
     desc "nvALT 2 is a fork of the original Notational Velocity with some additional features and interface modifications, including MultiMarkdown functionality."
     homepage "https://github.com/ttscoff/nv"
