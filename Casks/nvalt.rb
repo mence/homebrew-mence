cceb627c6bd9de27a71731f85a921ba5f482f34d
Tim Hordern <tim@timhordern.com>
2024-10-09 13:51:12 +1100

    Add nvALT 2 using Github repo information without unnecessary Rosetta requirement and deprecation warnings in upstream

---
+++ Casks/nvalt.rb
@@ -0,0 +1,25 @@
+cask "nvalt" do
+    version '2.2.8-128'
+    sha256 "85420c2a8d505a580b4aa4f0ef4662f08aa4af6139fb4ed448752b6b6e8fd671"
+
+    url "https://github.com/ttscoff/nv/releases/download/v#{version}/nvALT.#{version.no_hyphens}.dmg"
+    name "nvALT"
+    desc "nvALT 2 is a fork of the original Notational Velocity with some additional features and interface modifications, including MultiMarkdown functionality."
+    homepage "https://github.com/ttscoff/nv"
+
+    livecheck do
+      url :url
+      strategy :github_latest
+    end
+
+    auto_updates true
+    depends_on macos: ">= :high_sierra"
+
+    app "nvALT.app"
+
+    zap trash: [
+      "~/Library/Application Support/nVALT",
+      '~/Library/Preferences/net.elasticthreads.nv.plist',
+      '~/Library/Caches/net.elasticthreads.nv'
+    ]
+end
